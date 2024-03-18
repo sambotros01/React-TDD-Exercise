bf95908ec07d3a3df379b0e765b585e2633234c5
Sam <sambotros01@gmail.com>
2024-03-18 14:14:44 -0600

    3 out of 4 Passing Tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <>
-    <h1>My Recipes</h1>
-    <p>There are no recipes to list</p>
-    </>
-  )
-}
+  const [recipeFormShown, showRecipeForm] = useState(false);
+
+  let SubmitRecipe = (event) => {
+    event.preventDefault();
+
+    let newRecipeName = document.getElementById('newRecipeName').value;
+    let newRecipeInstructions = document.getElementById('newRecipeInstructions').value;
+
+    this.setState(
+      {recipes: [
+        {
+          name: newRecipeName,
+          instructions: newRecipeInstructions
+        }
+      ]
+    })
+  }
+    return(
+      <div className='App'>
+        <h1 className='App-Header'>My Recipes</h1>
+        <p>There are no recipes to list</p>
+      {
+        recipeFormShown ?
+        <>
+          <form id='recipe-form' name='recipe-form' onSubmit={SubmitRecipe}>
+            <label htmlFor='newRecipeName'>Recipe name: </label>
+            <input type='text' id='newRecipeName' />
+
+            <label htmlFor='newRecipeInstructions'>Recipe Instructions: </label>
+            <textarea id='newRecipeInstructions' placeholder='write recipe instructions here' />
+
+            <input type='submit' />
+          </form>
+        </>
+        :
+          <button onClick={ () => showRecipeForm(!recipeFormShown) }>Add Recipe</button>
+      }
+      </div>
+    )
+  }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('Chef should be able to view and store recipes', () => {
   render(<App />);
@@ -15,3 +17,68 @@ test('Chef should be able to view and store recipes', () => {
   // Recipe list is below Recipe header
   expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
 });
+
+test('Chef should be able to add recipes to their collection', () => {
+  render(<App />)
+
+  // Button that says "Add Recipe" below "My Recipes"
+  let recipeHeader = screen.getByText('My Recipes');
+  let recipeButton = screen.getByRole('button', {name: 'Add Recipe'});
+  expect(recipeButton).toBeInTheDocument();
+  expect(recipeHeader.compareDocumentPosition(recipeButton)).toBe(4);
+})
+
+test('When "Add Recipe" button is clicked, should show form with "Recipe Name" and "Recipe Instructions"', async () => {
+  render(<App />)
+
+  // Click Add Recipe Button
+  let recipeButton = screen.getByRole('button', {name: 'Add Recipe'});
+
+  act(() => {
+    userEvent.click(recipeButton);
+  })
+
+
+  // Wait for form to appear on screen
+  let form = await screen.findByRole('form', undefined, {timeout: 3000})
+
+  // Verify the form appears
+  expect(form).toBeInTheDocument();
+
+  // Form should have fields "Recipe Name" and "Recipe Instructions"
+  expect(screen.getByRole('textbox', {name: /Recipe name/i})).toBeInTheDocument();
+  expect(screen.getByRole('textbox', {name: /Recipe instructions/i})).toBeInTheDocument();
+
+  // "Add Recipe" button should not be on the screen once the form opens
+  recipeButton = screen.queryByRole('button', {name: 'Add Recipe'})
+  expect(recipeButton).toBeNull()
+})
+
+test('Chef should be able to see a recipe that has been added under "My Recipes"', async () => {
+  render(<App />)
+
+  // Click on "Add Recipe" button
+  let recipeButton = await screen.getByRole('button', {name: /Add Recipe/i});
+  userEvent.click(recipeButton)
+
+  // Wait for the form/textbox to appear
+  let recipeNameBox = await screen.findByRole('textbox', {name: /Recipe name/i});
+  let recipeInstructionBox =  await screen.findByRole('textbox', {name: /Recipe instructions/i});
+
+  // Add the recipe and instructions
+  const recipeName = 'Tofu Scramble Tacos';
+  const recipeInstructions = "1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas";
+  userEvent.type(recipeNameBox, recipeName);
+  userEvent.type(recipeInstructionBox, recipeInstructions);
+
+  // Click on the submit button
+  let submitButton = screen.getByRole('button');
+  userEvent.click(submitButton);
+
+  // Wait for text to appear
+  let recipe = screen.findByText(/Name:.*Tofu Scramble Tacos/i);
+
+})
+
+
+
